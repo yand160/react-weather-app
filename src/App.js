2be01f79ebc27912ec05af48f8cce3c9212d0d91
yand160 <74022340+yand160@users.noreply.github.com>
2023-01-30 18:37:51 -0500

    Added Github Url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return;
-  <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded with ❤️ by Yandia Alexis and is{" "}
+        <a href="https://github.com/yand160/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
