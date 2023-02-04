3debc8b8159d047159801674dbb182bbc9c6aa3c
yand160 <74022340+yand160@users.noreply.github.com>
2023-02-03 23:27:38 -0500

    added html and updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded with ❤️ by Yandia Alexis and is{" "}
-        <a href="https://github.com/yand160/react-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1></h1>
+        <Weather />
+        <footer>
+          This project was coded with ❤️ by Yandia Alexis and is{" "}
+          <a
+            href="https://github.com/yand160/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city...."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Friday 7:05</li>
+        <li>Partly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly Cloudy"
+          />
+          17°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 44%</li>
+            <li>Wind: 14 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
