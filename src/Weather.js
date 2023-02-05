4f22750643f5497f3d2d0eedd29ef67d104fd8bd
yand160 <74022340+yand160@users.noreply.github.com>
2023-02-05 18:03:31 -0500

    Added css styles

--- src/Weather.js
+++ src/Weather.js
@@ -9,12 +9,17 @@ export default function Weather() {
           <div className="col-9">
             <input
               type="search"
-              placeholder="Enter a city...."
+              placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,12 +29,20 @@ export default function Weather() {
         <li>Partly Cloudy</li>
       </ul>
       <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly Cloudy"
-          />
-          17°C
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Partly Cloudy"
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="temperature">17</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
