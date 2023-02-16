0519ba38c77511dc646b0083b91e35d0c4af5020
yand160 <74022340+yand160@users.noreply.github.com>
2023-02-15 19:02:11 -0500

    fixed missing information

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       temperature: response.data.temperature.current,
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
-      description: response.condition.description,
+      description: response.data.condition.description,
       iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       wind: response.data.wind.speed,
       city: response.data.city,
